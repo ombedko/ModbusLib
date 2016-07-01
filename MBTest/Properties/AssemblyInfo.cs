4372d071425058af39c4da2e4efdee4c0494b1d9
ombedko <ole@bedko.com>
2016-07-01 08:22:21 +0200

    Fixed initial upload

--- MBTest/Properties/AssemblyInfo.cs
+++ MBTest/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MBTest")]
+[assembly: AssemblyTitle("MBSerialTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MBTest")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("MBSerialTest")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5d2e64ba-3310-4a44-af04-7d84bcdbcfb1")]
+[assembly: Guid("5ada82a7-1dd1-4be8-91fd-65fc1dde8d36")]
 
 // Version information for an assembly consists of the following four values:
 //
